9e760c697e7f22d433220e6e50ae9105165d3c52
Avinash Sridhar <sridharavinash@github.com>
2017-02-25 14:54:46 -0500

    handle invalid inputs

--- versionCompare.go
+++ versionCompare.go
@@ -3,6 +3,7 @@ package versionCompare
 import (
 	"regexp"
 	"strconv"
+	"errors"
 )
 
 type Version struct{
@@ -10,6 +11,7 @@ type Version struct{
 	minor int
 	patch string
 }
+
 
 func (v1 *Version) compare(v2 *Version) int{
 	if v1.major > v2.major {
@@ -38,22 +40,34 @@ func (v1 *Version) compare(v2 *Version) int{
 		}
 	}
 
-	return 0
+	return -2
 }	
 
-func versionCompare(v1 string, v2 string) int{
-	va := extract(v1)
-	vb := extract(v2)
-	return va.compare(vb)
+func versionCompare(v1 string, v2 string) (int, error){
+	va, err := extract(v1)
+	if err != nil {
+		return -2, err
+	}
+	
+	vb, err2 := extract(v2)
+
+	if err2 != nil {
+		return -2, err2
+	}
+
+	return va.compare(vb), nil
 	
 }
 
-func extract(in string) *Version{
+func extract(in string) (*Version, error){
 	ver := new(Version)
 	r := regexp.MustCompile(`(\d+).(\d+).?(.*)`)
 	r2 := r.FindStringSubmatch(in)
+	if r2 == nil {
+		return ver, errors.New("Unable to parse input.")
+	}
 	ver.major,_ = strconv.Atoi(r2[1])
 	ver.minor,_ = strconv.Atoi(r2[2])
 	ver.patch = r2[3]
-	return ver
+	return ver, nil
 }
--- versionCompare_test.go
+++ versionCompare_test.go
@@ -5,36 +5,43 @@ import (
 )
 
 func TestLesser(t *testing.T) {
-	v := versionCompare("2.4", "2.5")
+	v,_ := versionCompare("2.4", "2.5")
 	if v != -1 {
 		t.Error("Expected -1, got", v)
 	}
 }
 
 func TestEqual(t *testing.T) {
-	v := versionCompare("2.4", "2.4")
+	v,_ := versionCompare("2.4", "2.4")
 	if v != 0 {
 		t.Error("Expected 0, got", v)
 	}
 }
 
 func TestGreater(t *testing.T) {
-	v := versionCompare("2.4.1", "2.4")
+	v , _:= versionCompare("2.4.1", "2.4")
 	if v != 1 {
 		t.Error("Expected 1, got", v)
 	}
 }
 
 func TestPatchGreaterAlpha(t *testing.T) {
-	v := versionCompare("2.4.1-version", "2.4.1-b")
+	v , _:= versionCompare("2.4.1-version", "2.4.1-b")
 	if v != 1 {
 		t.Error("Expected 1, got", v)
 	}
 }
 
 func TestPatchGreaterAlpha2(t *testing.T) {
-	v := versionCompare("2.4.2-version", "2.4.1-zeta")
+	v, _ := versionCompare("2.4.2-version", "2.4.1-zeta")
 	if v != 1 {
 		t.Error("Expected 1, got", v)
+	}
+}
+
+func TestPatchNondigitVersions(t *testing.T) {
+	_ , err:= versionCompare("a.b.c-version", "b.a.c-zeta")
+	if err == nil {
+		t.Error("Expected error 'Unable to parse input' , got", err)
 	}
 }
